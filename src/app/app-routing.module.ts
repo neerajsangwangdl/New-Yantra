2dbf1b8b1d80a999b260848636e8ce0cac5745d0
neerajsangwangdl <115098051+neerajsangwangdl@users.noreply.github.com>
2023-06-19 19:38:27 +0530

    new app strat today

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+    // App Routes goes here
+    {
+      path: 'customer',
+      loadChildren: () =>
+        import('./modules/user/user.module').then(m => m.UserModule)
+    },
+    { path: '**', redirectTo: 'products' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './modules/user/login/login.component';
+import { UserModule } from './modules/user/user.module';
+import { AppHeaderComponent } from './modules/shared/app-header/app-header/app-header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AppHeaderComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
